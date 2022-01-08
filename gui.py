6671cf03a054213964a78b1aba3efe23687a1eee
joeldelacalle <joeldelacalle@gmail.com>
2022-01-08 16:28:49 +0100

    Tiempo activo, pruebas

--- gui.py
+++ gui.py
@@ -1,10 +1,12 @@
-from guizero import App, Text, TextBox, PushButton
+from guizero import App, Text, TextBox, PushButton, Picture, system_config
 
 #Functions
 def say_my_name():
         welcome_message.value = my_name.value
 
 app = App("Locuras varias")
+
+#print(system_config.supported_image_types)
 
 #Text Widget
 welcome_message = Text(app, text="Siempre papi, nunca impapi", size= 30, font="Times New Roman", color="blue")
@@ -14,6 +16,9 @@ my_name = TextBox(app)
 
 #Push Button
 update_text = PushButton(app, command=say_my_name, text="Solo lo sabe el sabio")
+
+#Picture
+#my_pic = Picture(app, image="IOT-Proyect/dwarf_PNG32.png")
 
 app.display()
 
