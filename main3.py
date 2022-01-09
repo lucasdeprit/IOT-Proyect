cb817e55facca9a8251de890af53f58ff2f72434
lucasdeprit <lucasdeprit@gmail.com>
2022-01-09 22:58:05 +0100

    nas

--- main3.py
+++ main3.py
@@ -2,20 +2,23 @@
 import requests
 from timeit import default_timer as timer
 from grove.grove_ultrasonic_ranger import GroveUltrasonicRanger
+from seeed_dht import DHT
 import RPi.GPIO as GPIO
 import time
 import threading
 
 url = 'https://corlysis.com:8086/write'
-params = {"db": "Technology_Health_Computing", "u": "token", "p": "ecbef8c057c4ebac95399b37189bcf5e"}
+params = {"db": "Technology_Health_Computing", "u": "token", "p": "1dba55a4bca39916c0264efd2af30c63"}
 
 def myfunction():
     sensor = GroveUltrasonicRanger(16)
+    sensor2 = DHT('11', 5) 
     lejos=True
     medio=True
     cerca=True
     while True:
         distance = int(sensor.get_distance())
+        humi, temp = sensortemp.read()
         if distance >= 150 and lejos:
             print('lejos')
             print('{} cm'.format(distance))
@@ -25,6 +28,9 @@ def myfunction():
             if not cerca:
                 cerca=True
             payload = 'meas_test,distance=out' + ' value=' + str(distance)
+            r= requests.post(url,params=params,data=payload)
+
+            payload2 = 'temp_test,distance=actual' + ' value=' + str(distance)
             r= requests.post(url,params=params,data=payload)
     
         if distance >=50 and distance <150 and medio:
